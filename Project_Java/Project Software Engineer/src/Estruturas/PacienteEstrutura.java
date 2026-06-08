5ba4e28e81af418afd0575bc44c862bf55a09bda
Arthur Avila <arthuravila440@gmail.com>
2026-06-07 22:59:21 -0300

    Update PacienteEstrutura.java

--- Project_Java/Project Software Engineer/src/Estruturas/PacienteEstrutura.java
+++ Project_Java/Project Software Engineer/src/Estruturas/PacienteEstrutura.java
@@ -1,8 +1,25 @@
 package Estruturas;
 
-import Classes.Endereco;
 import Classes.Paciente;
+import java.util.ArrayList;
 
 public class PacienteEstrutura {
-    
+    private ArrayList<Paciente> lista;
+
+    public PacienteEstrutura() {
+        this.lista = new ArrayList<>();
+    }
+
+    public Paciente buscarPorCpf(String cpf) {
+        for (Paciente p : lista) {
+            if (p.getCpf().equals(cpf)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void adicionar(Paciente p) {
+        lista.add(p);
+    }
 }
